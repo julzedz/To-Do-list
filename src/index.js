e3a735de23e5348f32e402233a1109e0d7208c62
Edozie <edoziechukwuma@gmail.com>
2022-09-28 06:34:00 -0600

    Update files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,20 @@
 import "./style.css";
 
+const tasksArray = [
+  {
+    description: "Fix car",
+    completed: false,
+    id: 1,
+  },
+  {
+    description: "Wash dishes",
+    completed: false,
+    id: 2,
+  },
+  {
+    description: "Complete project",
+    completed: false,
+    id: 3
+  }
+];
+const itemList = document.querySelector('#item-list');
