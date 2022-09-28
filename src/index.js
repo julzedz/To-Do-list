6f077372eade3794e22ffbf27f56ab8cb8957943
Edozie <edoziechukwuma@gmail.com>
2022-09-28 07:24:06 -0600

    Update files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 const tasksArray = [
   {
     description: "Fix car",
-    completed: false,
+    completed: true,
     id: 1,
   },
   {
@@ -18,3 +18,18 @@ const tasksArray = [
   }
 ];
 const itemList = document.querySelector('#item-list');
+
+const populateHtml = () => {
+  itemList.innerHTML = tasksArray.map(
+    (data, index) => `<li class="items">
+          <div>
+            <input type="checkbox" class="todo-item" name="car">
+            <label for="car">${data.description}</label>
+          </div>
+          <svg class="w-2 h-2 option" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z">
+            </path>
+          </svg>
+        </li>`
+  );
+}
