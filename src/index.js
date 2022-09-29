12efdc7cf6e9deeaa8b1a9f55dc378dd478ce0f4
Edozie <edoziechukwuma@gmail.com>
2022-09-29 03:29:10 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import './style.css';
 import Todo from './modules/additem.js';
 
-const tasksArray = [];
+let tasksArray = [];
 const itemList = document.querySelector('#item-list');
 
 const populateHtml = () => {
   itemList.innerHTML = tasksArray.map(
-    (data) => `<li class="items">
+    (data, index) => `<li class="items">
           <div>
             <input type="checkbox" ${data.completed ? 'checked' : ''} class="todo-item" name="car">
             <label for="car">${data.description}</label>
           </div>
-          <svg class="w-2 h-2 option" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z">
+          <svg class="w-2 h-2 option"  onclick='removeTask(${index})' fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path id='option' stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z">
             </path>
           </svg>
         </li>`,
@@ -20,6 +20,12 @@ const populateHtml = () => {
 };
 
 populateHtml();
+
+const removeTask = (index) => {
+  tasksArray = tasksArray.filter((data, id) => id !== index);
+  populateHtml();
+};
+removeTask();
 
 const toDoInput = document.querySelector('#todo-input');
 
@@ -35,3 +41,7 @@ toDoInput.addEventListener('keypress', (e) => {
     toDoInput.value = '';
   }
 });
+
+// document.querySelector('#option').addEventListener('click', (e) => {
+//   removeTask(this.id);
+// });
