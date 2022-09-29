79c618895a9860b58f4827ccb1a89f00a866f370
Edozie <edoziechukwuma@gmail.com>
2022-09-29 07:36:32 -0600

    Add remove function

--- src/index.js
+++ src/index.js
@@ -10,10 +10,13 @@ const populateHtml = () => {
             <input type="checkbox" ${data.completed ? 'checked' : ''} class="todo-item" name="car">
             <label for="" contenteditable="true">${data.description}</label>
           </div>
-          <svg class="w-2 h-2 option"  onclick='removeTask(${index})' fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path id='option' stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z">
+          <div>
+            <svg class="w-2 h-2 option" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z">
             </path>
           </svg>
+          <button ${index} id="remove-btn">x</button>
+          </div>
         </li>`,
   ).join(' ');
 };
@@ -26,13 +29,22 @@ class Todo {
     this.description = description;
     this.completed = false;
   }
+
+  static updateIndex = () => {
+    tasksArray.forEach((data, index) => { data.index = index + 1; });
+  }
+
+  static removeTask = (index) => {
+    tasksArray.splice(index - 1, 1);
+    this.updateIndex();
+  }
 }
 
-const removeTask = (index) => {
-  tasksArray = tasksArray.filter((data, id) => id !== index);
-  populateHtml();
-};
-removeTask(Todo.id);
+// const removeTask = (index) => {
+//   tasksArray.filter((data, id) => id !== index);
+//   populateHtml();
+// };
+// removeTask(Todo.id);
 
 const toDoInput = document.querySelector('#todo-input');
 
@@ -46,7 +58,3 @@ toDoInput.addEventListener('keypress', (e) => {
     toDoInput.value = '';
   }
 });
-
-// document.querySelector('#option').addEventListener('click', (e) => {
-//   removeTask(this.id);
-// });
