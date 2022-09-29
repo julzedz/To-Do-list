30651995447fefa3beaae10b2661bee0e804b81e
Edozie <edoziechukwuma@gmail.com>
2022-09-29 06:28:34 -0600

    Update files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import './style.css';
-import Todo from './modules/additem.js';
 
-let tasksArray = [];
 const itemList = document.querySelector('#item-list');
+const tasksArray = JSON.parse(localStorage.getItem('todo')) || [];
 
 const populateHtml = () => {
   itemList.innerHTML = tasksArray.map(
     (data, index) => `<li class="items">
           <div>
             <input type="checkbox" ${data.completed ? 'checked' : ''} class="todo-item" name="car">
-            <label for="car">${data.description}</label>
+            <label for="" contenteditable="true">${data.description}</label>
           </div>
           <svg class="w-2 h-2 option"  onclick='removeTask(${index})' fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path id='option' stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z">
@@ -21,11 +20,19 @@ const populateHtml = () => {
 
 populateHtml();
 
+class Todo {
+  constructor(description) {
+    this.id = tasksArray.length + 1;
+    this.description = description;
+    this.completed = false;
+  }
+}
+
 const removeTask = (index) => {
   tasksArray = tasksArray.filter((data, id) => id !== index);
   populateHtml();
 };
-removeTask();
+removeTask(Todo.id);
 
 const toDoInput = document.querySelector('#todo-input');
 
@@ -33,9 +40,7 @@ toDoInput.addEventListener('keypress', (e) => {
   if (e.keyCode === 13) {
     e.preventDefault();
     const description = toDoInput.value;
-    const completed = false;
-    const id = Date.now();
-    const newToDo = new Todo(id, description, completed);
+    const newToDo = new Todo(description);
     tasksArray.push(newToDo);
     populateHtml();
     toDoInput.value = '';
--- src/modules/additem.js
+++
@@ -1,7 +0,0 @@
-export default class Todo {
-  constructor(id, description, completed) {
-    this.id = id;
-    this.description = description;
-    this.completed = completed;
-  }
-}
