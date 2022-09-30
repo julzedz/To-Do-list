e4fd7afb30cf1635270d13bdbcf97b7600d27671
Edozie <edoziechukwuma@gmail.com>
2022-09-30 11:00:26 -0600

    Add a module file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,9 @@
 import './style.css';
-import Todo from './modules/todo.js';
 import editFunction from './modules/edittask.js';
-import {
-  tasksArray, toDoInput,
-} from './modules/var.js';
 import populateHtml from './modules/populate.js';
+import inputMethod from './modules/input.js';
 
 populateHtml();
-
-const inputMethod = () => {
-  toDoInput.addEventListener('keypress', (e) => {
-    if (e.keyCode === 13) {
-      e.preventDefault();
-      const description = toDoInput.value;
-      const newToDo = new Todo(description);
-      tasksArray.push(newToDo);
-      populateHtml();
-      toDoInput.value = '';
-    }
-  });
-};
 
 inputMethod();
 
---
+++ src/modules/input.js
@@ -0,0 +1,17 @@
+import { tasksArray, toDoInput } from './var.js';
+import Todo from './todo.js';
+import populateHtml from './populate.js';
+
+const inputMethod = () => {
+  toDoInput.addEventListener('keypress', (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+      const description = toDoInput.value;
+      const newToDo = new Todo(description);
+      tasksArray.push(newToDo);
+      populateHtml();
+      toDoInput.value = '';
+    }
+  });
+};
+export default inputMethod;
